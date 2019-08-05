fc20648f49b7055e99d9b4c6954deecbdb74555d
amy <amy@nano.lan>
2019-08-05 13:30:50 -0400

    logic game

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,19 +1,42 @@
 console.log("Up and running!");
 
-var cards = ["Queen", "Queen", "king", "king"];
+var cards = [{
+rank: 'Queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: 'Queen',
+suit: 'Diamonds',
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: 'King',
+suit: 'Hearts',
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank: 'King',
+suit: 'Diamonds',
+cardImage: "images/king-of-diamonds.png",
+}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
   			console.log("You found a match!");
 		} else {
-  			console.log("Sorry, try again.");
+  			alert("Sorry, try again.");
 		}	
 }
 
 function flipCard(cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log(`User flipped ${cards[cardId]}`);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(`User flipped ${cards[cardId].rank}`);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	if(cardsInPlay.length === 2) { 
 		checkForMatch();
 	}
@@ -22,4 +45,4 @@ function flipCard(cardId) {
 
 
 flipCard(0);
-flipCard(1);
+flipCard(2);
