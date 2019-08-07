8100ec8312a852db978f68a3e625c924d58493a2
amy <amy@nano.lan>
2019-08-07 15:16:03 -0400

    logic game

--- memory_game/main.js
+++ memory_game/main.js
@@ -24,25 +24,41 @@ cardImage: "images/king-of-diamonds.png",
 
 var cardsInPlay = [];
 
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+    	var cardElement = document.createElement('img');
+    	cardElement.setAttribute('src', 'images/back.png');
+   		cardElement.setAttribute('data-id', i);
+   		//console.log("hello world")
+   		//console.log(flipCard)
+   		cardElement.addEventListener('click', flipCard);
+   		document.getElementById('game-board').appendChild(cardElement);
+  	}
+
+
+};
+
 function checkForMatch() {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-  			console.log("You found a match!");
+  			alert("You found a match!");
 		} else {
   			alert("Sorry, try again.");
 		}	
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	//console.log('flipcard called')
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(`User flipped ${cards[cardId].rank}`);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if(cardsInPlay.length === 2) { 
 		checkForMatch();
 	}
 }
 
 
+createBoard();
 
-flipCard(0);
-flipCard(2);
