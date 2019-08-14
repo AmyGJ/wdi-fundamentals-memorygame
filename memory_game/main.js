e53d05613057612c3a6f4abf3de9a44c5c3c412a
amy <amy@nano.lan>
2019-08-14 14:43:55 -0400

    logic game

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,22 +1,22 @@
 console.log("Up and running!");
 
 var cards = [{
-rank: 'Queen',
+rank: 'Queen 1',
 suit: 'hearts',
 cardImage: "images/queen-of-hearts.png"
 },
 {
-rank: 'Queen',
+rank: 'Queen 2',
 suit: 'Diamonds',
 cardImage: "images/queen-of-diamonds.png",
 },
 {
-rank: 'King',
+rank: 'King 1',
 suit: 'Hearts',
 cardImage: "images/king-of-hearts.png",
 },
 {
-rank: 'King',
+rank: 'King 2',
 suit: 'Diamonds',
 cardImage: "images/king-of-diamonds.png",
 }
@@ -43,10 +43,18 @@ function createBoard() {
 
 
 function checkForMatch() {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
+		if (cardsInPlay[0] === 'Queen 1' && cardsInPlay[1] === 'Queen 2') {
   			alert("You found a match!");
   			myScore+=1;
   			document.getElementById('score-box').innerHTML=myScore;
+		} else if (cardsInPlay[0] === 'Queen 1' && cardsInPlay[1] === 'Queen 1' || cardsInPlay[0] === 'Queen 2' && cardsInPlay[1] === 'Queen 2' ) {
+			alert("Cannot flip same card twice!")
+		} else if (cardsInPlay[0] === 'King 1' && cardsInPlay[1] === 'King 2') {
+  			alert("You found a match!");
+  			myScore+=1;
+  			document.getElementById('score-box').innerHTML=myScore;
+		} else if (cardsInPlay[0] === 'King 1' && cardsInPlay[1] === 'King 1' || cardsInPlay[0] === 'King 2' && cardsInPlay[1] === 'King 2') {
+			alert("Cannot flip same card twice!")
 		} else {
   			alert("Sorry, try again.");
 		}	
